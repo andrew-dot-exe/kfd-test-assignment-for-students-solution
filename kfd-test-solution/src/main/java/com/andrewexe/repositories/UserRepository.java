80f9b1d15544a9a7cfc1bb8ea3f991a1dd28f99d
Andrew <andrejnikitin974@gmail.com>
2025-08-17 01:11:51 +0400

    fix for abstract repository

---
+++ kfd-test-solution/src/main/java/com/andrewexe/repositories/UserRepository.java
@@ -0,0 +1,51 @@
+package com.andrewexe.repositories;
+
+import com.andrewexe.entities.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserRepository {
+
+    private String repositoryFilename = null;
+    private Map<Integer, User> users;
+
+    public UserRepository(){
+        users = new HashMap<Integer, User>();
+    }
+
+    public void loadData(String filename){
+        repositoryFilename = filename;
+        if(repositoryFilename == null){
+            //in-memory
+            addUser(new UserStudent(1, "Ivan Ivanov", "+799923320"));
+            addUser(new UserStudent(2, "Sergej Ivanov", "+799923321"));
+            addUser(new UserFaculty(3, "Andrej Sidorov", "+799923322"));
+            addUser(new UserFaculty(4, "Eugene Petrov", "+799923323"));
+            addUser(new UserGuest(5, "Ian Petrov", "+799923324"));
+
+        }
+    }
+
+    public void addUser(User user) throws IllegalArgumentException{
+        users.put(user.getUserId(), user);
+    }
+
+    public void removeUser(User user) throws IllegalArgumentException{
+        users.remove(user.getUserId());
+    }
+
+    public List<User> getAllUsers(){
+        return new ArrayList<User>(users.values());
+    }
+
+    public void save(){
+        if(repositoryFilename != null){
+            System.out.println("saving data to file");
+        }
+    }
+}
+
+
